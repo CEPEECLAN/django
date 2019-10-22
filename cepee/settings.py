9b8b0fff6fa36f5aa53b32516d5a9d24cddea311
Gabriel <gabrwagn@gmail.com>
2019-10-22 22:27:57 +0200

    Add localhost to allowed hosts... because that wasnt obvious

--- cepee/settings.py
+++ cepee/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'k_0qqz8jhk2@yz#!_ucbgkurfa%*y6ck&xpmtbh&^e0t1xmn6k'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.herokuapp.com']
+ALLOWED_HOSTS = ['.herokuapp.com', 'localhost']
 
 
 # Application definition
