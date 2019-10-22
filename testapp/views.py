d8578a93a264ed434d9303d64736418d6c5aaa31
Gabriel <gabrwagn@gmail.com>
2019-10-22 22:27:32 +0200

    Add cepee logo to websitegit statusgit statusgit status!

--- testapp/views.py
+++ testapp/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the testapp index.")
+    return HttpResponse('Hello, world. Youre at the testapp index. <br> <img src="/static/cepee_logo.png">')
